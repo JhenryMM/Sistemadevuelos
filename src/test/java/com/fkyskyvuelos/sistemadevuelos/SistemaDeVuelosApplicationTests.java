3e082c106b9c26a289bd2494641f94953d98338d
JhenryMM <65234337+JhenryMM@users.noreply.github.com>
2023-12-04 19:29:34 -0300

    Add files via upload

---
+++ src/test/java/com/fkyskyvuelos/sistemadevuelos/SistemaDeVuelosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fkyskyvuelos.sistemadevuelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeVuelosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
